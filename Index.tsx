07c414166a58d2a0bacb74b41bfff9c59f02a54b
shiriyang73-sketch <shiriyang73@gmail.com>
2026-02-14 22:45:36 +0600

    Create Index.tsx

---
+++ Index.tsx
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Happy Valentine's Day! 💕</h1>
+    </div>
+  );
+};
+
+export default Index;
